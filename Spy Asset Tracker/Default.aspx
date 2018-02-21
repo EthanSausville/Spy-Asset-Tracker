e54824c455f3b27dd9c5931222e569a217fc4be9
EthanSausville <ethan.sausville@gmail.com>
2018-02-21 09:01:29 -0800

    Upload

---
+++ Spy Asset Tracker/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Spy_Asset_Tracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <h1>Asset Performance Tracker</h1>
+        <p>
+            Asset Name:
+            <asp:TextBox ID="assetNameTextbox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Elections Rigged:
+            <asp:TextBox ID="electionsTextbox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="subterfugeTextbox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add Asset" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Spy Asset Tracker/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Spy_Asset_Tracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string[] assetNameArray;
+        int[] electionsRiggedArray;
+        int[] subterfugeArray;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assetNameArray = new string[0];
+                ViewState.Add("Asset", assetNameArray);
+
+                int[] electionsRiggedArray = new int[0];
+                ViewState.Add("Elections", electionsRiggedArray);
+
+                int[] subterfugeArray = new int[0];
+                ViewState.Add("Subterfuge", subterfugeArray);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string[] assetNameArray = (string[])ViewState["Asset"];
+            int[] electionsRiggedArray = (int[])ViewState["Elections"];
+            int[] subterfugeArray = (int[])ViewState["Subterfuge"];
+
+            Array.Resize(ref assetNameArray, assetNameArray.Length + 1);
+            Array.Resize(ref electionsRiggedArray, electionsRiggedArray.Length + 1);
+            Array.Resize(ref subterfugeArray, subterfugeArray.Length + 1);
+
+            int lastItem = assetNameArray.GetUpperBound(0);
+
+            assetNameArray[lastItem] = assetNameTextbox.Text;
+            electionsRiggedArray[lastItem] = int.Parse(electionsTextbox.Text);
+            subterfugeArray[lastItem] = int.Parse(subterfugeTextbox.Text);
+
+            ViewState["Asset"] = assetNameArray;
+            ViewState["Elections"] = electionsRiggedArray;
+            ViewState["Subterfuge"] = subterfugeArray;
+
+            resultLabel.Text = string.Format(
+                "Total Elections Rigged: {0}<br />" +
+                "Average acts of subterfuge per Asset: {1:N2}<br />" +
+                "(Last Asset Added: {2})",
+                electionsRiggedArray.Sum(), subterfugeArray.Average(), assetNameArray[lastItem]);
+
+            assetNameTextbox.Text = "";
+            electionsTextbox.Text = "";
+            subterfugeTextbox.Text = "";
+        }
+    }
+}
